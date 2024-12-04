db4dfb209b312aff7cfc59a34729d0e1b8d26387
Carlo Cano <99160767+carlocano03@users.noreply.github.com>
2024-12-04 18:04:45 +0800

    News API

---
+++ application/models/mobile/Api_news_update_model.php
@@ -0,0 +1,44 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+/**
+ *
+ * @version 1.0
+ * @author Carlo Cano <carlocano03@gmail.com>
+ * @copyright Copyright &copy; 2022,
+ *
+ */
+
+class Api_news_update_model extends MY_Model
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    function insert_news($insert_news)
+    {
+        $insert = $this->db->insert('news_update', $insert_news);
+        return $insert?TRUE:FALSE;
+    }
+
+    function add_news_comment($insert_comment)
+    {
+        $insert = $this->db->insert('news_comment', $insert_comment);
+        return $insert?TRUE:FALSE;
+    }
+
+    function get_all_news()
+    {
+        $this->db->select('NU.*, MI.member_no, MI.selfie_img');
+        $this->db->select("CONCAT(MI.first_name, ' ', MI.last_name) as posted_name");
+        $this->db->from('news_update NU');
+        $this->db->join('member_info MI', 'NU.user_id = MI.member_user_id', 'left');
+        $this->db->where('NU.is_deleted', NULL);
+        $this->db->where('NU.status', 0);
+        $query = $this->db->get();
+        return $query->result();
+    }
+
+}
